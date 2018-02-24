7a844d8c74814c73ee9da495dc7007720f9a5611
Daria Sobon <da.sobon@gmail.com>
2018-02-24 10:00:43 +0700

    Add files

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+
+
+var name = prompt('Enter your name');
+// alert('Hello, ' + name);
+console.log('Hello, ' + name);
+
+/*komentarz wieloliniowy 
+który może mieć wiele linii lol */
+
+//komentarz jednoliniowy
